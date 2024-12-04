6cb04fab6c5d63519d493aa2af3e8e0c7e7ebf44
Alex Muench <ammuench@gmail.com>
2024-12-03 23:17:45 -0600

    challenge 3 part 2 success!

--- challenge_03/challenge_03.go
+++ challenge_03/challenge_03.go
@@ -6,36 +6,85 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
-  part1()
+	part1()
+	part2()
 }
 
 func part1() {
-  inputData:= parseInputData()
+	inputData := parseInputData()
 
-  regex:= regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	regex := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
 
-  matches := regex.FindAllStringSubmatch(inputData, -1)
+	matches := regex.FindAllStringSubmatch(inputData, -1)
 
-  multSum := 0
+	multSum := 0
 
-  if (len(matches) > 0) {
-    for _, match := range matches {
-      mult1, err := strconv.Atoi(match[1])
-      mult2, err := strconv.Atoi(match[2])
+	if len(matches) > 0 {
+		for _, match := range matches {
+			mult1, err := strconv.Atoi(match[1])
+			mult2, err := strconv.Atoi(match[2])
 
-      if err != nil {
-        log.Fatal(err)
-      } else {
-        multSum += mult1*mult2
-      }
-    }   
-  }
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				multSum += mult1 * mult2
+			}
+		}
+	}
 
-  fmt.Printf("The result of all the matches mults is %v\n", multSum)
+	fmt.Printf("The result of all the matches mults is %v\n", multSum)
+}
 
+func part2() {
+	inputData := parseInputData()
+	searchData := inputData
+	doCmds := ""
+
+	findDoBlock := false // Commands start enabled
+
+	for {
+		if findDoBlock {
+			splitResults := strings.SplitN(searchData, "do()", 2)
+			findDoBlock = false
+			if len(splitResults) < 2 {
+				break
+			} else {
+				searchData = splitResults[1]
+			}
+		} else {
+			splitResults := strings.SplitN(searchData, "don't()", 2)
+			findDoBlock = true
+			doCmds = doCmds + splitResults[0]
+			if len(splitResults) < 2 {
+				break
+			} else {
+				searchData = splitResults[1]
+			}
+		}
+	}
+
+	multSum := 0
+		secondRegex := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+		secondMatches := secondRegex.FindAllStringSubmatch(doCmds, -1)
+
+		if len(secondMatches) > 0 {
+			for _, subMatch := range secondMatches{
+				mult1, err := strconv.Atoi(subMatch[1])
+				mult2, err := strconv.Atoi(subMatch[2])
+
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					multSum += mult1 * mult2
+				}
+			}
+		}
+
+	fmt.Printf("The result of all the do-matches mults is %v\n", multSum)
 }
 
 func parseInputData() string {
